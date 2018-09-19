d0ea09fff48cdbff9fac062a9b3962e17399cda3
lichuanlei <799441030@qq.com>
2018-09-19 11:21:15 +0800

    提交文件

---
+++ src/main/java/com/szmirren/vxApi/VxApplication.java
@@ -0,0 +1,9 @@
+package com.szmirren.vxApi;
+
+import com.szmirren.vxApi.core.VxApiLauncher;
+
+public class VxApplication {
+    public static void main(String[] args) {
+    	VxApiLauncher.start();
+	}
+}
